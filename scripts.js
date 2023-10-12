9568512c77447735b79e9e99de94eabca8a195ba
Ogechiprecious <ogenwaprecious97@gmail.com>
2023-10-12 04:26:39 +0100

    init

---
+++ scripts.js
@@ -0,0 +1,32 @@
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav = document.getElementById('nav-bar');
+
+if (bar) {
+    bar.addEventListener('click', () => {
+        nav.classList.add('active');
+    })
+}
+
+if (close) {
+    close.addEventListener('click', () => {
+        nav.classList.remove('active');
+    })
+}
+
+var mainImg = document.getElementById('mainImg');
+var smallimg = document.getElementsByClassName('small-img');
+
+smallimg[0].onclick = function(){
+    mainImg.src = smallimg[0].src;
+}
+smallimg[1].onclick = function(){
+    mainImg.src = smallimg[1].src;
+}
+smallimg[2].onclick = function(){
+    mainImg.src = smallimg[2].src;
+}
+smallimg[3].onclick = function(){
+    mainImg.src = smallimg[3].src;
+}
+
